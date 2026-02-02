7eb31fd0b4260629f73174fc6ec4eeb58d927614
powdercat <86944385+powdercat@users.noreply.github.com>
2026-02-02 20:47:04 +0900

    [Silver V] Title: 배열 합치기, Time: 1600 ms, Memory: 413648 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,6 +1,4 @@
-
 import java.io.*;
-//import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String args[]) throws Exception {
@@ -14,46 +12,29 @@ public class Main {
         String[] mArray_st = br.readLine().split(" ");
 
         int len = N + M;
-//        int[] result = new int[len];
         int ni = 0, mi = 0;
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < len; i++) {
             if (ni >= N) {
                 bw.write(mArray_st[mi++] + " ");
-//                result[i] = Integer.parseInt(mArray_st[mi]);
-//                System.out.print(mArray_st[mi++] + " ");
                 continue;
             } else if (mi >= M) {
                 bw.write(nArray_st[ni++] + " ");
-//                result[i] = Integer.parseInt(nArray_st[ni]);
-//                System.out.print(nArray_st[ni++] + " ");
                 continue;
             }
             int nv = Integer.parseInt(nArray_st[ni]);
             int mv = Integer.parseInt(mArray_st[mi]);
             if (nv < mv) {
                 ni++;
-//                result[i] = nv;
                 bw.write(nv + " ");
-//                System.out.print(nv + " ");
             } else {
                 mi++;
-//                result[i] = mv;
                 bw.write(mv + " ");
-//                System.out.print(mv + " ");
             }
         }
 
-           //버퍼에 있는 값 전부 출력
-        bw.flush();   //남아있는 데이터를 모두 출력시킴
-        bw.close();   //스트림을 닫음
-//        System.out.print(result[0]);
-
-//        String s = result[0] + "";
-//        for (int i = 1; i < len; i++) {
-//            s += (" " + result[i]);
-////            System.out.print(" " + result[i]);
-//        }
+        bw.flush();
+        bw.close();
     }
 }
