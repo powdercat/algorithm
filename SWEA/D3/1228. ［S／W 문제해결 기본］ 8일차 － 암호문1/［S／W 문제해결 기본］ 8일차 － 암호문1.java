201be6909b8b93bd72d00f69f3d0335cd15a5452
powdercat <86944385+powdercat@users.noreply.github.com>
2026-02-02 15:33:15 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 92 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			LinkedList<Integer> origin = new LinkedList<>();
+			int N = Integer.parseInt(br.readLine());
+			
+			String[] input = br.readLine().split(" ");
+			for (String s : input) {
+				origin.add(Integer.parseInt(s));
+			}
+			
+			int addN = Integer.parseInt(br.readLine());
+			String[] insertCommand = br.readLine().split(" ");
+			
+			int index = 0;
+			for (int i = 0; i < addN;) {
+				if (insertCommand[index].equals("I")) {
+					int insertIndex = Integer.parseInt(insertCommand[++index]);
+					int k = Integer.parseInt(insertCommand[++index]);
+					for (int j = 0; j < k; j++) {
+						origin.add(insertIndex++, Integer.parseInt(insertCommand[++index]));
+					}
+					index++;
+					i++;
+				}
+			}
+			System.out.printf("#%d", test_case);
+			for (int i = 0; i < 10; i++) {
+				System.out.print(" " + origin.get(i));
+			}
+			System.out.println();
+		}
+
+	}
+
+}
