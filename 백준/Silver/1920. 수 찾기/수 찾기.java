59e187a254ee3fe67ea6a2cebf54fd186c2f5fc0
powdercat <86944385+powdercat@users.noreply.github.com>
2026-02-15 20:07:53 +0900

    [Silver IV] Title: 수 찾기, Time: 544 ms, Memory: 60400 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,37 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int[] numbers;
+
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = Integer.parseInt(br.readLine());
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int M = Integer.parseInt(br.readLine());
         int[] finds = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        // 이분탐색을 위해 numbers 정렬
         Arrays.sort(numbers);
 
         for (int target : finds) {
-            int left = 0;
-            int right = N - 1;
+            int result = binarySearch(target);
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-            boolean find = false;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (numbers[mid] == target) {
-                    find = true;
-                    break;
-                } else if (numbers[mid] < target) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-            if (find) {
-                System.out.println(1);
+    private static int binarySearch(int target) {
+        int left = 0;
+        int right = N - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (numbers[mid] == target) {
+                return 1;
+            } else if (numbers[mid] < target) {
+                left = mid + 1;
             } else {
-                System.out.println(0);
+                right = mid - 1;
             }
         }
+
+        return 0;
     }
 }
