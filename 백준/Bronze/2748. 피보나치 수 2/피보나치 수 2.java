4377c22eaf6755444133df2ea9e781e1b70d640a
powdercat <86944385+powdercat@users.noreply.github.com>
2026-02-23 20:27:06 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 68 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] fib = new long[N + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        System.out.println(fib[N]);
+    }
+}
