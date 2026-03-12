e3f15a3b8cfe1ef450831243a3429772413dde40
powdercat <86944385+powdercat@users.noreply.github.com>
2026-03-12 10:26:36 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 178 ms, Memory: 51,124 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	
-	static int[] parents;
 
     public static void main(String[] args) throws Exception {
 
@@ -12,6 +10,7 @@ public class Solution {
     	int TC = Integer.parseInt(br.readLine());
     	for (int tc = 1; tc <= TC; tc++) {
     		int N = Integer.parseInt(br.readLine());
+    		boolean[] visited = new boolean[N];
     		
     		int[] x = new int[N];
     		int[] y = new int[N];
@@ -22,59 +21,51 @@ public class Solution {
     			y[i] = Integer.parseInt(yInput[i]);
     		}
     		double E = Double.parseDouble(br.readLine());
-    		
-    		double[][] edges = new double[N * (N - 1)][3];
-    		int k = 0;
+
+    		// 정점 i -> j, j -> i 들의 연결 간선 정보 저장
+    		double[][] adjMatrix = new double[N][N];
     		for (int i = 0; i < N; i++) {
     			for (int j = 0; j < N; j++) {
     				if (i == j) continue;
     				double w = (Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2)) * E;
-    				edges[k++] = new double[] {i, j, w};
+    				adjMatrix[i][j] = w;
     			}
     		}
     		
-    		Arrays.sort(edges, (e1, e2) -> Double.compare(e1[2], e2[2]));
-    		
-    		parents = new int[N];
-    		for (int i = 0; i < N; i++) {
-    			parents[i] = -1;
-    		}
+    		double[] minEdge = new double[N];
+    		Arrays.fill(minEdge, Double.MAX_VALUE);
+    		minEdge[0] = 0;
     		
     		double result = 0;
-    		int cnt = 0;
-    		for (double[] edge : edges) {
-    			int a = (int) edge[0];
-    			int b = (int) edge[1];
-    			double w = edge[2];
-    			if (union(a, b)) {
-    				result += w;
-    				cnt++;
-    				if (cnt == N - 1) break;
+    		for (int v = 0; v < N; v++) { // 정점 수 만큼 정점을 선택하기
+    			double min = Double.MAX_VALUE;
+    			int minVertex = -1;
+    			
+    			// 현재의 MST와 연결할 수 있는 정점 중 간선 비용이 최소인 정점 찾기
+    			for (int i = 0; i < N; i++) {
+    				if (!visited[i] && min > minEdge[i]) {
+    					min = minEdge[i];
+    					minVertex = i;
+    				}
     			}
     			
+    			if (minVertex == -1) {
+    				break;
+    			}
+    			
+    			visited[minVertex] = true;
+    			result += min;
+    			
+    			// minVertex를 MST에 연결하며, minVertex와 연결된 비트리정점의 정보를 활용해 minEdge 갱신
+    			for (int i = 0; i < N; i++) {
+    				if (visited[i]) continue;
+    				if (adjMatrix[minVertex][i] == 0) continue;
+    				if (minEdge[i] > adjMatrix[minVertex][i]) {
+    					minEdge[i] = adjMatrix[minVertex][i];
+    				}
+    			}
     		}
     		System.out.println("#" + tc + " " + Math.round(result));
     	}
     }
-    
-    static int findRoot(int a) {
-    	if (parents[a] < 0) return a;
-    	return parents[a] = findRoot(parents[a]);
-    }
-
-	private static boolean union(int a, int b) {
-		int aRoot = findRoot(a);
-		int bRoot = findRoot(b);
-		if (aRoot == bRoot) return false;
-		
-		if (parents[aRoot] <= parents[bRoot]) {
-			parents[aRoot] += parents[bRoot];
-			parents[bRoot] = aRoot;
-		} else {
-			parents[bRoot] += parents[aRoot];
-			parents[aRoot] = bRoot;
-		}
-		
-		return true;
-	}
 }
