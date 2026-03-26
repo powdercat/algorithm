dc834ba73de8e16ae6fe3b3e7f599eb32f914cd4
powdercat <86944385+powdercat@users.noreply.github.com>
2026-03-26 17:38:24 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 92 ms, Memory: 12320 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -6,34 +6,33 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		// dp 0~N까지 전부 초기 길이는 1
 		int N = Integer.parseInt(br.readLine());
 		String[] input = br.readLine().split(" ");
+		
 		int[] A = new int[N];
 		int[] dp = new int[N];
+		int[] prev = new int[N];
 		
 		for (int i = 0; i < N; i++) {
 			A[i] = Integer.parseInt(input[i]);
-			dp[i] = 1;
 		}
+		Arrays.fill(dp, 1);
+		Arrays.fill(prev, -1);
+		
 		if (N == 1) {
 			System.out.println(1);
 			System.out.println(A[0]);
 			System.exit(0);
 		}
 
-		int count = 0;
-		int maxIdx = -1;
+		int count = 1;
+		int maxIdx = 0;
 		for (int i = 1; i < N; i++) {
-			int prevMaxDp = -1;
 			for (int j = 0; j < i; j++) {
-				//
-				if (A[j] < A[i]) {
-					prevMaxDp = Math.max(prevMaxDp, dp[j]);
+				if (A[j] < A[i] && dp[j] + 1 > dp[i]) {
+					dp[i] = dp[j] + 1;
+					prev[i] = j;
 				}
-			}
-			if (prevMaxDp != -1) {
-				dp[i] = prevMaxDp + 1;
 			}
 			if (dp[i] > count) {
 				count = dp[i];
@@ -43,15 +42,10 @@ public class Main {
 		System.out.println(count);
 		
 		Deque<Integer> stack = new ArrayDeque<>();
-		stack.push(A[maxIdx]);
-		count--;
-		maxIdx--;
 		while (count > 0) {
-			if (count == dp[maxIdx] && A[maxIdx] < stack.peek()) {
-				stack.push(A[maxIdx]);
-				count--;
-			}
-			maxIdx--;
+			stack.push(A[maxIdx]);
+			maxIdx = prev[maxIdx];
+			count--;
 		}
 		while (!stack.isEmpty()) {
 			System.out.print(stack.pop() + " ");
